397fdd34ed88fe8af73f0b8e6ea011a45f471cf4
Ullynacuaria <84741271+Ullynacuaria@users.noreply.github.com>
2022-07-11 17:14:52 -0500

    operators

---
+++ Operators/operators.js
@@ -0,0 +1,47 @@
+//Basic Arithmetic operators
+let x = 60
+let y = 20
+console.log("Subtraction of x from y = ", x - y);
+console.log("Product of x from y = ", x * y);
+console.log("Division of x from y = ", x / y);
+console.log("Modulus of x from y = ", x % y);//get the remainder
+
+//Increment Operators pre and post
+console.log("Value of x = ", x);
+console.log("Printing out x++ = ", x++);
+
+console.log("Value of x = ", x);
+console.log("Printing out ++x = ", ++x);
+
+//Decrement operators pre and post
+console.log("Value of x = ", x);
+console.log("Printing out x-- = ", x--);
+
+console.log("Value of x = ", x);
+console.log("Printing out --x = ", --x);
+
+//Increment/Decrement and assign
+y = x;
+console.log("Value of x is assigned to y = ", y);
+x += 1;
+console.log("Value of x after x += ", x);
+y -= 1;
+console.log("Value of y after y -= 1", y);
+
+//Comparasion and Operators
+console.log("Comparasion / Operators")
+
+let intValue = 70;
+let stringValue = "70"
+
+//Values comparasion
+console.log("intValue == stringValue is ", intValue == stringValue);
+console.log("intValue === stringValue is ", intValue === stringValue);
+console.log("intValue != stringValue is ", intValue != stringValue);
+console.log("intValue !== stringValue is ", intValue !== stringValue);
+
+//Greater and less then
+console.log("intValue > stringValue is ", intValue > stringValue);
+console.log("intValue < stringValue is ", intValue < stringValue);
+console.log("intValue >= stringValue is ", intValue >= stringValue);
+console.log("intValue <= stringValue is ", intValue <= stringValue);
