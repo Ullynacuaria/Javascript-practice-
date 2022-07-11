c852f5c6bfc92ead8089d38eae33fd14f7851248
Ullynacuaria <84741271+Ullynacuaria@users.noreply.github.com>
2022-07-11 17:48:10 -0500

    logical.js

---
+++ Logical/logical.js
@@ -0,0 +1,28 @@
+//Basic logical Js
+
+console.log("Logical Operators");
+let A = 10;
+let B = 15;
+
+let X = 10;
+let Y = 15
+
+//and
+if(A >= 5 && B <= 15)
+console.log("A + B = ", A + B);
+
+//Or
+if(X > 15 || Y < 20)
+console.log("X + Y = ", X + Y);
+ 
+//Not
+let result = !(X == Y);
+console.log("Result of !(X == Y) = ", result);
+
+//Conditions in js
+let condition;
+console.log("Condition = ", condition = (100 > 10) ? true : false);
+console.log("Condition = ", condition = (100 < 10) ? true : false);
+
+let maxValue;
+console.log("maxValue = ", condition = (100 > 10) ? 100 : 10);
