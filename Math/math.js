e71861bf7695092ddeb970a3f80573848e447760
Ullynacuaria <84741271+Ullynacuaria@users.noreply.github.com>
2022-07-11 14:01:57 -0500

    math.js

---
+++ Math/math.js
@@ -0,0 +1,41 @@
+//Some of the constants available in the Js library
+console.log("Value of Pi: ", Math.PI);
+console.log("The Euler's constant is: ", Math.E);
+
+//Using the round Function
+console.log("Rounded value of 3.2: ", Math.round(3.2));
+console.log("Rounded value of 3.5: ", Math.round(3.5));
+console.log("Rounded value of 3.8: ", Math.round(3.8));
+
+//Using the abs function to get the absolute value
+console.log("Absolute value of 4: ", Math.abs(4));
+console.log("Absolute value of -4: ", Math.abs(-4));
+
+//Using the pow function to get the power value
+console.log("4 to the 17th power: ", Math.pow(4,17));
+
+//Using sqrt to get the square root of the values
+console.log("Square root of 20.08: ", Math.sqrt(20.08));
+console.log("Square root of 42: ", Math.sqrt(42));
+
+//Rounding Down to the nearst interger
+console.log("Floor value of 2.6: ", Math.floor(2.6));
+console.log("Floor value of 2.4: ", Math.floor(2.4));
+
+//Rounding Up to the nearst interger
+console.log("Cealing value of 2.6: ", Math.ceil(2.4));
+console.log("Cealing value of 2.4: ", Math.ceil(2.4));
+
+//Find the Max and MIN value in a sequence of numbers
+console.log("Highest number from the list: ", Math.max(10, 100, -200, 50));
+console.log("Lowest number from the list: ", Math.min(10, 100, -200, 50));
+
+//Using Trigonometric functions
+console.log("sin(30) = ", Math.sin(30 * Math.PI / 180));
+console.log("sin(30) = ", Math.cos(30 * Math.PI / 180));
+
+//Generating random value 
+console.log("Random Value 0-1 = ", Math.random());
+console.log("Random Value 0-9 = ", Math.floor(Math.random() * 10));
+console.log("Random Value 0-10 = ", Math.floor(Math.random() * 11));
+console.log("Random Value 1-10 = ", Math.floor(Math.random() * 10) + 1);
